491791e8ffc4b287a829164fc0dbb386f4905a14
bekzod-fayzikuloff <fayzikuloff.b@gmail.com>
2022-05-08 15:41:43 +0300

    Backend: postgresql integrated

--- backend/src/secret_santa/settings.py
+++ backend/src/secret_santa/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = "secret_santa.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": 5432,
     }
 }
 
